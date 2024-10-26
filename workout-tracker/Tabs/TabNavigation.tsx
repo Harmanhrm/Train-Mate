c90bc8aff3428c8e9c664a3b29dd6c2760cea407
Harmanhrm <harmanbyte@gmail.com>
2024-10-26 23:57:12 +1100

    updated tabnav to  include the logintab

--- workout-tracker/Tabs/TabNavigation.tsx
+++ workout-tracker/Tabs/TabNavigation.tsx
@@ -1,13 +1,35 @@
+
+// Updated TabNavigation.js
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import WorkoutsToday from './WorkoutsTodayTab';
 import CategoriesTab from './CategoriesTab';
 import StatisticsTab from './StatisticsTab';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = ({ userDetails }) => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Workouts Today') {
+            iconName = focused ? 'fitness' : 'fitness-outline';
+          } else if (route.name === 'Categories') {
+            iconName = focused ? 'list' : 'list-outline';
+          } else if (route.name === 'Statistics') {
+            iconName = focused ? 'stats-chart' : 'stats-chart-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007AFF',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
       <Tab.Screen name="Workouts Today">
         {props => <WorkoutsToday {...props} userDetails={userDetails} />}
       </Tab.Screen>
@@ -15,7 +37,7 @@ const TabNavigation = ({ userDetails }) => {
         {props => <CategoriesTab {...props} userDetails={userDetails} />}
       </Tab.Screen>
       <Tab.Screen name="Statistics">
-        {props => <StatisticsTab {...props}  userDetails={userDetails}/>}
+        {props => <StatisticsTab {...props} userDetails={userDetails} />}
       </Tab.Screen>
     </Tab.Navigator>
   );
